338a10b0ef08763ed830c36052b71a15f52beed1
dcoderk <webmarket2k@gmail.com>
2019-04-24 16:38:32 +0800

    fixing nav bar menu and default page

--- footer.php
+++ footer.php
@@ -11,26 +11,138 @@
 
 ?>
 
-	</div><!-- #content -->
+</div><!-- #content start from header-->
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'redback-theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'redback-theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'redback-theme' ), 'redback-theme', '<a href="http://underscores.me/">Jay Ramilo</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+ <!-- Footer -->
+<footer class="footer-bg font-small  pt-4">
+
+<!-- Footer Links -->
+<div class="container text-center text-md-left">
+
+  <div class="row p-4">
+
+    <div class="col-md-5 mt-md-0 mt-3">
+      <span class="footer-heading">About Us</span>
+      <p class="about-description">Redback Conferencing is an Australian conferencing and digital <br>
+events provider dedicated to helping organisations meet, present <br>
+and collaborate at a distance.</p>
+
+       <div class="f-list1"> 
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Leadership Team</a>
+          </li>
+          <li>
+            <a href="#!">Meet the Family</a>
+          </li>
+          <li>
+            <a href="#!"> Social Responsibility</a>
+          </li>
+          <li>
+            <a href="#!">Contact Us</a>
+          </li>
+          <li>
+            <a href="#!">Privacy Statement</a>
+          </li>
+        </ul>
+      </div>  
+
+      <!--Social buttons-->
+    <div class="social-icon">
+      <a><i class="fab fa-twitter fa-lg"> </i></a>
+      <a><i class="fab fa-instagram fa-lg"></i></a>
+      <a><i class="fab fa-facebook-f fa-lg"> </i></a>
+      <a><i class="fab fa-youtube fa-lg"></i></a>
+    </div>
+    <!--/.Social buttons-->
+
+    </div>
+
+    <hr class="clearfix w-100 d-md-none pb-3">
+
+    <div class="col-md-3 mb-md-0 mb-3 services-list">
+       <span class="footer-heading">Services</span>
+       <span> 
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Teleconferencing</a>
+          </li>
+          <li>
+            <a href="#!">Web Conferencing</a>
+          </li>
+          <li>
+            <a href="#!">Video Conferencing</a>
+          </li>
+          <li>
+            <a href="#!">Webinars</a>
+          </li>
+          <li>
+            <a href="#!">Webcasts</a>
+          </li>
+          <li>
+            <a href="#!">Digital Event Consulting</a>
+          </li>
+        </ul>
+      </span>  
+      
+      <div class="footer-heading f-list2"> Resources</div>
+       <span> 
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Resources</a>
+          </li>
+          <li>
+            <a href="#!">Support Centre</a>
+          </li>
+          <li>
+            <a href="#!">Redbacktalk Blog</a>
+          </li>
+        </ul>
+      </span>  
+    </div>
+    
+    <div class="col-md-4 mb-md-0 mb-3 contact-info">
+        <span class="footer-heading"> CONTACT US</span>
+        <p class="footer-heading"><i class="fas fa-map-marker-alt fa-lg"></i> Sydney Office:</p>
+        <p>Level 21, 320 Pitt Street, Sydney NSW 200<br>
+          (02) 8014 5150
+        </p>
+      
+        <p class="footer-heading"><i class="fas fa-map-marker-alt fa-lg"></i> Melbourne Office:</p>
+        <p>Level 7, 460 Collins Street (Entrance off William St.)<br>
+            Melbourne VIC 3000 <br>
+            (03) 9034 3960
+        </p>
+      
+        <div class="footer-heading"><i class="fas fa-phone fa-lg"></i> AUSTRALIAN TOLL FREE: 1800 733-416</div>
+        <div class="footer-heading"><i class="fas fa-headset fa-lg"></i> NZ TOLL FREE: 0800 231 717</div>
+        <div class="footer-heading"><i class="fas fa-user-friends"></i> OVERSEAS: +61 2 9037 0330</div>
+      
+    </div>
+
+  </div>
+
+</div>
+<!-- Footer Links -->
+
+</footer>
+<!-- Footer -->
+
+
+<!-- Copyright -->
+<div class="copyright">
+  <div class="container">
+  <div class="row">
+    <div class="col">
+    <div class="py-3">Copyright 2019. Designed by Pounce Marketing. All Rights Reserved.</div>
+    </div>
+  </div>
+  </div>
+</div>
+<!-- Copyright -->
+
 
 <?php wp_footer(); ?>
-
 </body>
 </html>
+
--- header.php
+++ header.php
@@ -18,15 +18,60 @@
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
+
+	<style>
+
+	@media only screen and (min-width: 1024px) { 
+		.site-header {
+			height: 30vh;
+		}
+	} 
+
+	@media (min-width: 560px) and (max-width: 650px) {
+	header .jarallax h1 {
+		margin-bottom: .5rem !important;
+	}
+	header .jarallax h5 {
+		margin-bottom: .5rem !important;
+	}
+	}
+
+
+	@media (min-width: 660px) and (max-width: 700px) {
+	header .jarallax h1 {
+		margin-bottom: 1.5rem !important;
+	}
+	header .jarallax h5 {
+		margin-bottom: 1.5rem !important;
+	}
+	}
+
+	
+	.navbar:not(.top-nav-collapse) {
+		background: transparent !important;
+	}
+
+
+	@media (min-width: 800px) and (max-width: 850px) {
+		.navbar:not(.top-nav-collapse) {
+			background: #9da4b1!important;
+		}
+	} 
+
+	
+	</style>
+
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'redback-theme' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
+<header>
+
+	<!--Navbar-->
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top scrolling-navbar custom-nav">
+	<div class="container">
+		<a class="navbar-brand" href="#">
+		<?php
 			the_custom_logo();
 			if ( is_front_page() && is_home() ) :
 				?>
@@ -41,18 +86,57 @@
 			if ( $redback_theme_description || is_customize_preview() ) :
 				?>
 				<p class="site-description"><?php echo $redback_theme_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'redback-theme' ); ?></button>
-			<?php
+		<?php endif; ?>
+		</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-7"
+		aria-controls="navbarSupportedContent-7" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent-7">
+		<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
+				'menu_class'     => 'navbar-nav nav-list mr-auto',
 			) );
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+		?>
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item">
+			<a class="nav-link" href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/Asset_A1.png"  alt="product" class="product-icon" /></a>
+			</li>
+			<li class="nav-item">
+			<a class="nav-link" href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/Asset_A2.png"  alt="product" class="product-icon"/></a>
+			</li>
+			<li class="nav-item">
+			<a class="nav-link" href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/Asset_A3.png"  alt="product" class="product-icon"/></a>
+			</li>
+		</ul>
+		</div>
+	</div>
+	</nav>
 
-	<div id="content" class="site-content">
+	<!-- Intro Section -->
+	<div class="view jarallax site-header" data-jarallax='{"speed": 0.2}' style="background-image: url('http://redback.test/wp-content/uploads/2019/04/Header1.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
+	<div class="mask rgba-purple-slight">
+		<div class="container h-100 d-flex justify-content-center align-items-center">
+		<div class="row pt-5 mt-3">
+			<div class="col-md-12 wow fadeIn mb-3">
+			<div class="text-center">
+				<h2 class="heading-title font-weight-bold mb-5 wow fadeInUp white-text">Collaboration Services</h2>
+				</li>
+				<h6 class="mb-5 heading-subtitle wow fadeInUp font-weight-bold white-text" data-wow-delay="0.2s">For All Your Day-to-day Meetings</h6>
+				</ul>
+			</div>
+			</div>
+		</div>
+		</div>
+	</div>
+	</div>
+
+</header>
+<!--Main Navigation-->
+
+
+
+
+<div id="content" class="site-content">
+	
